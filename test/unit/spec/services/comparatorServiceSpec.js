667249979d3f03e693b7b51654c4f2d74f55f2ee
Carlos Oliveira <candreoliveira@gmail.com>
2014-07-25 23:39:46 -0300

    Add travis ci

--- test/unit/spec/services/comparatorServiceSpec.js
+++ test/unit/spec/services/comparatorServiceSpec.js
@@ -12,7 +12,24 @@ describe('ComparatorService', function (){
     });
   });
 
-  it('testing', function(){
-    expect( 'a' ).toBe( 'a' );
+
+  describe('Min function', function (){
+    it('should return min lenght of two arrays', function(){
+      var a = [1,2,3],
+          b = [0,5];
+
+      expect( service.min(a,b) ).toEqual( 2 );
+
+    });
+  });
+
+  describe('Max function', function (){
+    it('should return max lenght of two arrays', function(){
+      var a = [1,2,3],
+          b = [0,5];
+
+      expect( service.max(a,b) ).toEqual( 3 );
+
+    });
   });
 });
