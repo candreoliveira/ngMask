436b275fbdc92eb7f1b0ebc60bf58748b2217411
Carlos Oliveira <candreoliveira@gmail.com>
2014-07-26 02:17:34 -0300

    Service test

--- test/unit/spec/services/selectorServiceSpec.js
+++ test/unit/spec/services/selectorServiceSpec.js
@@ -1,5 +1,31 @@
 describe('SelectorService', function (){
-  var service;
+  var service, patterns = {
+    '9': /[0-9]/,
+    '8': /[0-8]/,
+    '7': /[0-7]/,
+    '6': /[0-6]/,
+    '5': /[0-5]/,
+    '4': /[0-4]/,
+    '3': /[0-3]/,
+    '2': /[0-2]/,
+    '1': /[0-1]/,
+    '0': /[0]/,
+    '*': /./,
+    'w': /\w/,
+    'W': /\w/,
+    'd': /\d/,
+    'D': /\D/,
+    's': /\s/,
+    'S': /\S/,
+    'b': /\b/,
+    'A': /[A-Z]/,
+    'a': /[a-z]/,
+    'Z': /[A-ZÇÀÁÂÃÈÉÊẼÌÍÎĨÒÓÔÕÙÚÛŨ]/,
+    'z': /[a-zçáàãâéèêẽíìĩîóòôõúùũüû]/,
+    '@': /[a-zA-Z]/,
+    '#': /[a-zA-ZçáàãâéèêẽíìĩîóòôõúùũüûÇÀÁÂÃÈÉÊẼÌÍÎĨÒÓÔÕÙÚÛŨ]/,
+    '%': /[0-9a-zA-zçáàãâéèêẽíìĩîóòôõúùũüûÇÀÁÂÃÈÉÊẼÌÍÎĨÒÓÔÕÙÚÛŨ]/
+  };
 
   beforeEach(function(){
     angular.module('test', ['ngMask']);
@@ -7,8 +33,9 @@ describe('SelectorService', function (){
   });
 
   beforeEach(function (){
-    inject(function(SelectorService) {
+    inject(function(SelectorService, PatternService) {
       service = SelectorService;
+      patternService = PatternService;
     });
   });
 
@@ -58,5 +85,42 @@ describe('SelectorService', function (){
       expect(range.moveStart).toHaveBeenCalledWith('character', 0);
       expect(range.moveEnd).toHaveBeenCalledWith('character', 1);
     });
+  });
+
+  describe('setCaretPosition function', function (){
+    var Input = function (){};
+    Input.prototype.setSelectionRange = function (s, e){};
+    Input.prototype.focus = function (){};
+
+    it('should set apply caret', function(){
+      var input  = new Input(),
+          mask   = '33333',
+          values = '33334';
+
+      spyOn(input, 'setSelectionRange');
+      service.setCaretPosition(input, mask, values, patterns);
+      expect(input.setSelectionRange).toHaveBeenCalledWith(4, 5);
+    });
+
+    it('should set apply caret', function(){
+      var input  = new Input(),
+          mask   = '39/19/9999',
+          values = '49/19/9999';
+
+      spyOn(input, 'setSelectionRange');
+      service.setCaretPosition(input, mask, values, patterns);
+      expect(input.setSelectionRange).toHaveBeenCalledWith(0, 1);
+    });
+
+    it('should set apply caret', function(){
+      var input  = new Input(),
+          mask   = '119.961.307',
+          values = '119a';
+
+      spyOn(input, 'setSelectionRange');
+      service.setCaretPosition(input, mask, values, patterns);
+      expect(input.setSelectionRange).toHaveBeenCalledWith(4, 5);
+    });
+
   });
 });
